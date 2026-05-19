09652715b849f655e240546991b20326a9441dca
aar0njv <aaronjoy382@gmail.com>
2026-05-19 17:54:16 +0530

    smtp-update

--- smtp/client.c
+++ smtp/client.c
@@ -1,46 +1,48 @@
 #include <stdio.h> 
 #include <string.h> 
+#include <unistd.h>
 #include <sys/socket.h> 
 #include <netinet/in.h> 
 #include <arpa/inet.h> 
 #include <stdlib.h> 
-#define MAX 100 
+
  
-void exchange(int sd, char *msg, struct sockaddr_in *addr) { 
-    char buf[MAX]; 
-    sendto(sd, msg, MAX, 0, (struct sockaddr *)addr, sizeof(*addr)); 
-    int n = recvfrom(sd, buf, MAX, 0, NULL, NULL); 
-    buf[n] = '\0'; 
-    printf("S: %s", buf); 
-} 
- 
-int main(int argc, char *argv[]) { 
-    int sd; 
-    struct sockaddr_in servaddr; 
-    char addr_in[MAX], buf[MAX]; 
-    sd = socket(AF_INET, SOCK_DGRAM, 0); 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_port = htons(atoi(argv[2])); 
-    inet_pton(AF_INET, argv[1], &servaddr.sin_addr); 
-    // Start Handshake 
-    exchange(sd, "Connection Request", &servaddr); 
-    exchange(sd, "HELO client.com\n", &servaddr); 
-    printf("Enter Sender Email: "); 
-    scanf("%s", addr_in); 
-    sprintf(buf, "MAIL FROM: <%s>\n", addr_in); 
-    exchange(sd, buf, &servaddr); 
-    printf("Enter Recipient Email: "); 
-    scanf("%s", addr_in); 
-    sprintf(buf, "RCPT TO: <%s>\n", addr_in); 
-    exchange(sd, buf, &servaddr); 
-    exchange(sd, "DATA\n", &servaddr); 
-    printf("Enter Mail Body (end with '.' on a new line):\n"); 
-    while (1) { 
-        fgets(buf, MAX, stdin); 
-        sendto(sd, buf, MAX, 0, (struct sockaddr *)&servaddr, sizeof(servaddr)); 
-        if (strncmp(buf, ".", 1) == 0) break; 
-    } 
-    exchange(sd, "QUIT\n", &servaddr); 
-    close(sd); 
-    return 0; 
+int main() { 
+    int server, client;
+    struct sockaddr_in serverAddr, clientAddr;
+    char buffer[1024];
+    socklen_t len;
+
+    client = socket(AF_INET, SOCK_STREAM, 0);
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverAddr.sin_port = htons(8080);
+
+    connect(client, 
+            (struct sockaddr*)&serverAddr, 
+            sizeof(serverAddr));
+    
+    printf("Connected to SMTP Server...\n");
+
+    while(1) {
+        memset(buffer, 0, sizeof(buffer));
+
+        printf("Enter the SMTP Command: ");
+        fgets(buffer, sizeof(buffer), stdin);
+        send(client, buffer, strlen(buffer), 0);
+
+        memset(buffer, 0, sizeof(buffer));
+
+        recv(client, buffer, sizeof(buffer), 0);
+        printf("Server: %s", buffer);
+
+        if (strncmp(buffer, "221", 3) == 0) {
+            break;
+        }
+    }
+
+    close(client);
+    return 0;
+
 }
--- smtp/server.c
+++ smtp/server.c
@@ -1,54 +1,74 @@
 #include <stdio.h> 
+#include <stdlib.h>
 #include <string.h> 
 #include <sys/socket.h> 
 #include <netinet/in.h> 
-#include <stdlib.h> 
-#define MAX 1024
+#include <unistd.h>
  
-int main(int argc, char *argv[]) { 
-    int sock_fd, n; 
-    struct sockaddr_in servaddr, cliaddr; 
-    char buffer[MAX]; 
-    socklen_t len = sizeof(cliaddr); 
-    sock_fd = socket(AF_INET, SOCK_DGRAM, 0);     
-    memset(&servaddr, 0, sizeof(servaddr)); 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_port = htons(atoi(argv[1])); 
-    servaddr.sin_addr.s_addr = INADDR_ANY; 
-    bind(sock_fd, (struct sockaddr *)&servaddr, sizeof(servaddr)); 
-    printf("SMTP Server Waiting on Port %s...\n", argv[1]); 
-    // 1. Connection Request 
-    recvfrom(sock_fd, buffer, MAX, 0, (struct sockaddr *)&cliaddr, &len); 
-    sendto(sock_fd, "220 Welcome to Mail Server\n", MAX, 0, (struct sockaddr *)&cliaddr, 
-len); 
+int main() { 
+    int server, client;
+    struct sockaddr_in serverAddr, clientAddr;
+    socklen_t len;
+    char buffer[1024];
  
-    // 2. HELO 
-    n = recvfrom(sock_fd, buffer, MAX, 0, (struct sockaddr *)&cliaddr, &len); 
-    buffer[n] = '\0'; printf("C: %s", buffer); 
-    sendto(sock_fd, "250 Hello Client\n", MAX, 0, (struct sockaddr *)&cliaddr, len); 
-    // 3. MAIL FROM 
-    n = recvfrom(sock_fd, buffer, MAX, 0, (struct sockaddr *)&cliaddr, &len); 
-    buffer[n] = '\0'; printf("C: %s", buffer); 
-    sendto(sock_fd, "250 Sender OK\n", MAX, 0, (struct sockaddr *)&cliaddr, len); 
-    // 4. RCPT TO 
-    n = recvfrom(sock_fd, buffer, MAX, 0, (struct sockaddr *)&cliaddr, &len); 
-    buffer[n] = '\0'; printf("C: %s", buffer); 
-    sendto(sock_fd, "250 Recipient OK\n", MAX, 0, (struct sockaddr *)&cliaddr, len); 
-    // 5. DATA 
-    n = recvfrom(sock_fd, buffer, MAX, 0, (struct sockaddr *)&cliaddr, &len); 
-    sendto(sock_fd, "354 Start mail input; end with <CRLF>.<CRLF>\n", MAX, 0, (struct 
-sockaddr *)&cliaddr, len); 
- 
-    // 6. Receive Mail Content 
-    while (1) { 
-        n = recvfrom(sock_fd, buffer, MAX, 0, (struct sockaddr *)&cliaddr, &len); 
-        buffer[n] = '\0'; 
-        printf("Mail Data: %s", buffer); 
-        if (strncmp(buffer, ".", 1) == 0) break; 
-    } 
-    // 7. QUIT 
-    sendto(sock_fd, "221 Service closing transmission channel\n", MAX, 0, (struct sockaddr 
-*)&cliaddr, len);   
-    close(sock_fd); 
+    server = socket(AF_INET, SOCK_STREAM, 0);
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(8080);
+
+    bind(server, 
+        (struct sockaddr*)&serverAddr,
+        sizeof(serverAddr));
+    
+    listen(server, 5);
+    printf("SMTP Server waiting for Client...\n");
+
+    len = sizeof(clientAddr);
+
+    client = accept(server,
+                    (struct sockaddr*)&clientAddr,
+                    &len);
+    
+    printf("Client connected Successfully...\n");
+
+    while(1) {
+        memset(buffer, 0, sizeof(buffer));
+
+        //SMTP Command
+        recv(client, buffer, sizeof(buffer), 0);
+        printf("Client: %s", buffer);
+
+        //Quit condition
+        if (strncmp(buffer, "QUIT", 4) == 0) {
+            strcpy(buffer, "221 Connection Closed.\n");
+            send(client, buffer, strlen(buffer), 0);
+            break;
+        }
+
+        if(strncmp(buffer, "HELO", 4) == 0) {
+            strcpy(buffer, "250 Hello CLient.\n");
+        }
+        else if(strncmp(buffer, "MAIL FROM", 10) == 0) {
+            strcpy(buffer, "250 Sender OK.\n");
+        }
+        else if(strncmp(buffer, "RCPT TO", 7) == 0) {
+            strcpy(buffer, "250 Recipient OK.\n");
+        }
+        else if(strncmp(buffer, "DATA", 4) == 0) {
+            strcpy(buffer, "354 Enter Message: \n");
+        }
+        else {
+            strcpy(buffer, "250 Message Accepted.\n");
+        }
+
+        send(client, buffer, strlen(buffer), 0);
+
+    }
+    
+    
+    close(server);
+    close(client);
+
     return 0; 
 }
