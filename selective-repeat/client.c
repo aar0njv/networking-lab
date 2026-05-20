818a0e002bbf7f082cc893f7b65e86a211d9a56e
aar0njv <aaronjoy382@gmail.com>
2026-05-20 20:35:41 +0530

    selective-update

--- selective-repeat/client.c
+++ selective-repeat/client.c
@@ -1,34 +1,81 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <sys/socket.h> 
-#include <netinet/in.h> 
-#include <arpa/inet.h> 
-#include <string.h> 
-#include <time.h> 
-#include <unistd.h> 
- 
-int main() { 
-    srand(time(0)); 
-    int c_sock = socket(AF_INET, SOCK_STREAM, 0); 
-    struct sockaddr_in serv = {AF_INET, htons(9009), inet_addr("127.0.0.1")}; 
-    connect(c_sock, (struct sockaddr*)&serv, sizeof(serv)); 
-    char buff[100], reply[50]; 
-    int count = 0; 
- 
-    while (count < 9) { 
-        read(c_sock, buff, sizeof(buff)); 
-        int id = buff[strlen(buff)-1] - '0'; 
-        printf("Received: %s", buff); 
-        if (rand() % 4 == 0) { // 25% chance of corruption 
-            printf(" [Corrupted]\n"); 
-            sprintf(reply, "negative akwn-%d", id); 
-        } else { 
-            printf(" [OK]\n"); 
-            sprintf(reply, "akwnowledgementof-%d", id); 
-            count++; 
-        } 
-        write(c_sock, reply, sizeof(reply)); 
-    } 
-    close(c_sock); 
-    return 0; 
-} 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+
+int main() {
+
+    int client;
+    struct sockaddr_in serverAddr;
+    int windowSize = 4;
+    int frames[10];
+    int ack;
+    int received[10] = {0};
+    int i, j;
+
+    // Create socket
+    client = socket(AF_INET, SOCK_STREAM, 0);
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(8080);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    connect(client,
+            (struct sockaddr*)&serverAddr,
+            sizeof(serverAddr));
+
+    printf("Sender Started...\n");
+
+    // Timeout setup
+    struct timeval tv;
+
+    tv.tv_sec = 2;
+    tv.tv_usec = 0;
+
+    setsockopt(client, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+
+    // Initialize frames
+    for(i = 0; i < 10; i++) {
+        frames[i] = i;
+    }
+
+    int base = 0;
+
+    while(base < 10) {
+
+        // Send window frames
+        for(i = base; i < base + windowSize && i < 10; i++) {
+
+            // Send only unacknowledged frames
+            if(received[i] == 0) {
+                send(client, &frames[i], sizeof(frames[i]), 0);
+                printf("Sent Frame : %d\n", frames[i]);
+            }
+        }
+
+        // Receive ACKs
+        for(j = base; j < base + windowSize && j < 10; j++) {
+
+            int n = recv(client, &ack, sizeof(ack), 0);
+            if(n < 0) {
+                printf("Timeout for Frame %d\n", j);
+            }
+
+            else {
+                printf("ACK %d Received\n", ack);
+                received[ack] = 1;
+            }
+        }
+
+        // Slide window
+        while(received[base] == 1) {
+            base++;
+        }
+        printf("\n");
+    }
+
+    close(client);
+    return 0;
+}
--- selective-repeat/server.c
+++ selective-repeat/server.c
@@ -1,55 +1,82 @@
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <netinet/in.h> 
-#include <string.h> 
-#include <unistd.h> 
-#include <sys/time.h> 
- 
-void resend(int msg_id, int sock) { 
-    char buf[50]; 
-    sprintf(buf, "server message :%d", msg_id); 
-    printf("Resending Message %d\n", msg_id); 
-    write(sock, buf, sizeof(buf)); 
-} 
- 
-int main() { 
-    int s_sock, c_sock, tot = 0; 
-    struct sockaddr_in server, client; 
-    socklen_t len = sizeof(client); 
-    char buff[50]; 
-    s_sock = socket(AF_INET, SOCK_STREAM, 0); 
-    server.sin_family = AF_INET; 
-    server.sin_port = htons(9009); 
-    server.sin_addr.s_addr = INADDR_ANY; 
-    bind(s_sock, (struct sockaddr *)&server, sizeof(server)); 
-    listen(s_sock, 5); 
-    c_sock = accept(s_sock, (struct sockaddr *)&client, &len); 
- 
-    while (tot < 9) { 
-        // Send Window of 3 
-        for (int i = tot; i < tot + 3 && i < 9; i++) { 
-            sprintf(buff, "server message :%d", i); 
-            write(c_sock, buff, sizeof(buff)); 
-            usleep(1000); 
-        } 
-        // Wait for 3 individual ACKs 
-        for (int k = tot; k < tot + 3 && k < 9; k++) { 
-        wait_ack: 
-            fd_set set; 
-            struct timeval timeout = {2, 0}; 
-            FD_ZERO(&set); FD_SET(c_sock, &set); 
-            if (select(c_sock + 1, &set, NULL, NULL, &timeout) == 0) { 
-                resend(k, c_sock); // Timeout 
-                goto wait_ack; 
-            } else { 
-                read(c_sock, buff, sizeof(buff)); 
-                if (buff[0] == 'n') { // Negative ACK 
-                    resend(buff[strlen(buff) - 1] - '0', c_sock); 
-                    goto wait_ack; 
-                } else tot++; 
-            } 
-        } 
-    } 
-    close(c_sock); close(s_sock); 
-    return 0; 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<time.h>
+
+int main() {
+
+    int server, client;
+
+    struct sockaddr_in serverAddr, clientAddr;
+
+    socklen_t len;
+
+    int frame, ack;
+
+    srand(time(NULL));
+
+    // Create socket
+    server = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Server configuration
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(8080);
+
+    // Bind
+    bind(server,
+         (struct sockaddr*)&serverAddr,
+         sizeof(serverAddr));
+
+    // Listen
+    listen(server, 5);
+
+    printf("Receiver Waiting...\n");
+
+    len = sizeof(clientAddr);
+
+    // Accept
+    client = accept(server,
+                    (struct sockaddr*)&clientAddr,
+                    &len);
+
+    printf("Connection Established...\n");
+
+    while(1) {
+
+        // Receive frame
+        recv(client,
+             &frame,
+             sizeof(frame),
+             0);
+
+        // Random loss simulation
+        if(rand() % 10 < 2) {
+
+            printf("Frame %d Lost\n\n", frame);
+
+            continue;
+        }
+
+        printf("Received Frame : %d\n", frame);
+
+        // ACK same frame
+        ack = frame;
+
+        send(client,
+             &ack,
+             sizeof(ack),
+             0);
+
+        printf("ACK %d Sent\n\n", ack);
+    }
+
+    close(client);
+    close(server);
+
+    return 0;
 }
